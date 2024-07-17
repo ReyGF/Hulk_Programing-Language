17809ad55e0b94974fca2f20608eac581df21d51
ReyGF <gomezreynol14@gmail.com>
2024-07-17 18:23:35 -0400

    Update congif

--- obj/Debug/net8.0/Hulk_Programing-Language.AssemblyInfo.cs
+++ obj/Debug/net8.0/Hulk_Programing-Language.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hulk_Programing-Language")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17446216d38bf2cc23bc0f6292a6318cfe939f4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42ec5cbee8c00a4be6bd50206c2507a8780922b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hulk_Programing-Language")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hulk_Programing-Language")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
